a97a3d99837b3d455081b31b9ea7b2b4d14fc178
Piyush Kumar <68659006+PiyushKumar-18-AI@users.noreply.github.com>
2023-02-09 18:19:05 +0530

    Creating, Dropping and Altering of DB

--- Databases/query-1.sql
+++ Databases/query-1.sql
@@ -1,1 +1,11 @@
+/* For Creating a Database */
+
 CREATE DATABASE pkDB;
+
+/* For Dropping or Deleting a Database */
+
+Drop DATABASE pkDB
+
+/* For Altering a Database to Read Only Mode */
+
+ALTER DATABASE pkDB READ ONLY = 1;
